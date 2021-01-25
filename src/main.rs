f97a69b02614c9ec6643238d5db054cc4052e606
Alex Plate <AlexPl292@gmail.com>
2021-01-25 21:39:55 +0300

    Initial backup file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use grammers_client::{Client, Config};
+use grammers_tl_types as tl;
+use simple_logger::SimpleLogger;
+use tokio::task;
+use grammers_session::FileSession;
+use grammers_client::types::Chat;
+use std::fs::File;
+use std::io::Write;
+use grammers_tl_types::Serializable;
+
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Debug)
+        .init()
+        .unwrap();
+
+    let api_id = env!("TG_ID").parse().expect("TG_ID invalid");
+    let api_hash = env!("TG_HASH").to_string();
+
+    println!("Connecting to Telegram...");
+    let mut client = Client::connect(Config {
+        session: FileSession::load_or_create("dialogs.session").unwrap(),
+        api_id,
+        api_hash: api_hash.clone(),
+        params: Default::default(),
+    }).await.unwrap();
+    println!("Connected!");
+
+    let mut client_handle = client.handle();
+    task::spawn(async move { client.run_until_disconnected().await });
+
+    let mut dialogs = client_handle.iter_dialogs();
+
+    let mut found_chat: Option<Chat> = None;
+    while let Some(dialog) = dialogs.next().await.unwrap() {
+        let chat = dialog.chat();
+        if chat.id() == 59061750 {
+            found_chat = Some(chat.clone());
+        }
+    }
+
+    let nic_chat = found_chat.unwrap();
+    let mut messages = client_handle.iter_messages(&nic_chat).limit(100);
+
+    let mut res: Vec<String> = vec![];
+    while let Some(message) = messages.next().await.unwrap() {
+        res.push(message.text().to_string());
+    }
+
+    let string_res = serde_json::to_string_pretty(&res).unwrap();
+    let mut file = File::create("rust_backup.txt").unwrap();
+    file.write_all(string_res.as_bytes()).unwrap();
+}
+
+fn get_peer() -> tl::enums::InputPeer {
+    let user_id: i32 = env!("TG_USER_ID").parse().expect("TG_USER_ID invalid");
+    let access_hash: i64 = env!("TG_ACCESS_HASH")
+        .parse()
+        .expect("TG_ACCESS_HASH invalid");
+
+    let peer = tl::types::InputPeerUser {
+        user_id,
+        access_hash,
+    };
+    let peer_into = peer.into();
+    peer_into
 }
