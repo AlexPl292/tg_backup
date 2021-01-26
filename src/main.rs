83158882e081d351129998aacd2701c860ab82cd
Alex Plate <AlexPl292@gmail.com>
2021-01-26 23:59:54 +0300

    Extract dialog backing up into a separate method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod types;
 
-use crate::types::{MessageInfo, chat_to_info, msg_to_info};
-use grammers_client::{Client, Config};
+use crate::types::{chat_to_info, msg_to_info, MessageInfo};
+use grammers_client::types::Dialog;
+use grammers_client::{Client, ClientHandle, Config};
 use grammers_mtproto::mtp::RpcError;
 use grammers_mtsender::InvocationError;
 use grammers_session::FileSession;
@@ -46,52 +47,56 @@ async fn main() {
     let mut chat_index = 0;
     while let Some(dialog) = dialogs.next().await.unwrap() {
         chat_index += 1;
-        let chat = dialog.chat();
-        let path_str = make_path(chat.name(), chat_index);
-        let path = Path::new(path_str.as_str());
-        fs::create_dir_all(path).unwrap();
-        let info_file = path.join("info.json");
-        let file = File::create(info_file).unwrap();
-        serde_json::to_writer_pretty(&file, &chat_to_info(chat)).unwrap();
-
-        let data_file = path.join("data.json");
-        let mut file = File::create(data_file).unwrap();
-        let mut ser = serde_json::Serializer::new(std::io::Write::by_ref(&mut file));
-        let mut messages = client_handle.iter_messages(chat);
-        let total = messages.total().await.ok();
-        let mut seq = ser.serialize_seq(total).unwrap();
-        loop {
-            let msg = messages.next().await;
-            match msg {
-                Ok(Some(message)) => {
-                    log::debug!("Write element");
-                    let message_info: MessageInfo = msg_to_info(&message);
-                    seq.serialize_element(&message_info).unwrap();
-                }
-                Ok(None) => {
-                    break;
-                }
-                Err(InvocationError::Rpc(RpcError {
-                    code: _,
-                    name,
-                    value,
-                })) => {
-                    if name == "FLOOD_WAIT" {
-                        log::info!("Flood wait: {}", value.unwrap());
-                        sleep(Duration::from_secs(value.unwrap() as u64))
-                    } else {
-                        break;
-                    }
-                }
-                Err(e) => {
-                    log::error!("Error {}", e);
-                    break;
-                }
-            };
-        }
-        seq.end().unwrap();
-        log::info!("Finish writing data: {}", chat.name());
+        extract_dialog(&client_handle, chat_index, dialog).await;
     }
+}
+
+async fn extract_dialog(client_handle: &ClientHandle, chat_index: i32, dialog: Dialog) {
+    let chat = dialog.chat();
+    let path_str = make_path(chat.name(), chat_index);
+    let path = Path::new(path_str.as_str());
+    fs::create_dir_all(path).unwrap();
+    let info_file = path.join("info.json");
+    let file = File::create(info_file).unwrap();
+    serde_json::to_writer_pretty(&file, &chat_to_info(chat)).unwrap();
+
+    let data_file = path.join("data.json");
+    let mut file = File::create(data_file).unwrap();
+    let mut ser = serde_json::Serializer::new(std::io::Write::by_ref(&mut file));
+    let mut messages = client_handle.iter_messages(chat);
+    let total = messages.total().await.ok();
+    let mut seq = ser.serialize_seq(total).unwrap();
+    loop {
+        let msg = messages.next().await;
+        match msg {
+            Ok(Some(message)) => {
+                log::debug!("Write element");
+                let message_info: MessageInfo = msg_to_info(&message);
+                seq.serialize_element(&message_info).unwrap();
+            }
+            Ok(None) => {
+                break;
+            }
+            Err(InvocationError::Rpc(RpcError {
+                code: _,
+                name,
+                value,
+            })) => {
+                if name == "FLOOD_WAIT" {
+                    log::info!("Flood wait: {}", value.unwrap());
+                    sleep(Duration::from_secs(value.unwrap() as u64))
+                } else {
+                    break;
+                }
+            }
+            Err(e) => {
+                log::error!("Error {}", e);
+                break;
+            }
+        };
+    }
+    seq.end().unwrap();
+    log::info!("Finish writing data: {}", chat.name());
 }
 
 fn make_path(name: &str, id: i32) -> String {
