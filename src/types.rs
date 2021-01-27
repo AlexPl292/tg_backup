fba0629c4d02b0a1cbd0852c06cf7dab2ca2f480
Alex Plate <AlexPl292@gmail.com>
2021-01-27 15:56:53 +0300

    Save message date

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,8 @@
 use grammers_client::types::{Chat, Message};
 use serde::{Deserialize, Serialize};
 
+use chrono::{DateTime, Utc};
+
 #[derive(Serialize, Deserialize)]
 pub struct ChatInfo {
     name: String,
@@ -11,12 +13,14 @@ pub struct ChatInfo {
 pub struct MessageInfo {
     text: String,
     id: i32,
+    date: DateTime<Utc>,
 }
 
 pub fn msg_to_info(data: &Message) -> MessageInfo {
     MessageInfo {
         text: data.text().to_string(),
         id: data.id(),
+        date: data.date(),
     }
 }
 
