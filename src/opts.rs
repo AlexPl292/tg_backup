ff1c8a2b352912a824d731e67c756bdffd14cc19
Alex Plate <AlexPl292@gmail.com>
2021-03-24 10:23:29 +0300

    Add opts file back

---
+++ src/opts.rs
@@ -0,0 +1,23 @@
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(author = "Alex Plate <AlexPl292@gmail.com>")]
+pub struct Opts {
+    #[clap(short, long)]
+    pub included_chats: Vec<i32>,
+    #[clap(long, default_value = "1000")]
+    pub batch_size: i32,
+    #[clap(long)]
+    pub clean: bool,
+    #[clap(subcommand)]
+    pub auth: Option<SubCommand>,
+}
+
+#[derive(Clap)]
+pub enum SubCommand {
+    #[clap(version = "0.1.0", author = "Alex Plate <AlexPl292@gmail.com>")]
+    Auth(Auth),
+}
+
+#[derive(Clap)]
+pub struct Auth {}
