45bba3e8056925d9841997dc81d20128da8a9ee2
alex.plate <alex.plate>
2021-04-27 23:04:01 +0300

    Update version

--- build.sh
+++ build.sh
@@ -19,5 +19,7 @@
 #
 
 CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc cargo build --release --target=x86_64-unknown-linux-gnu
+zip -r target/x86_64-unknown-linux-gnu/release/tg_backup-x86_64-unknown-linux-gnu.zip target/x86_64-unknown-linux-gnu/release/tg_backup
 cargo build --release --target x86_64-pc-windows-gnu
+zip -r target/x86_64-pc-windows-gnu/release/tg_backup-x86_64-pc-windows-gnu.zip target/x86_64-pc-windows-gnu/release/tg_backup.exe
 cargo build --release --target x86_64-apple-darwin
